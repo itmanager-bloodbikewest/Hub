80751c1a80130420323e15791935a75b26dc2251
itmanager-bloodbikewest <itmanager@bloodbikewest.ie>
2026-07-19 11:49:45 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window.HUB_CONFIG = {
-  environment: "development",
-  showEnvBanner: true,
-  apps: {
-    rota: "https://dev.rota.bloodbikewest.ie",
-    control: "https://dev.app.bloodbikewest.ie"
-  }
-};
